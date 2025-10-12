73d79b6862fd13f9accf92e6c409b2469b353132
fzyzcjy <ch271828n@outlook.com>
2025-10-12 20:55:16 +0800

    more

--- miles/rollout/base_types.py
+++ miles/rollout/base_types.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, Optional
 from miles.utils.types import Sample
 
 
+# TODO may make input dataclass too to allow extensibility
 @dataclass
 class RolloutFnCallOutput:
     samples: Optional[list[list[Sample]]] = None

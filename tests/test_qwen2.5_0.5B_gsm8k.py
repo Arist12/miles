cf6ccd1505ef91bcc758dd58f927fcd7da6f0f1d
fzyzcjy <ch271828n@outlook.com>
2025-10-13 21:13:48 +0800

    disable eval

--- tests/test_qwen2.5_0.5B_gsm8k.py
+++ tests/test_qwen2.5_0.5B_gsm8k.py
@@ -37,7 +37,7 @@ def execute():
     )
 
     eval_args = (
-        "--eval-interval 20 "
+        # "--eval-interval 20 "
         "--eval-prompt-data gsm8k /root/datasets/gsm8k/test.parquet "
         "--n-samples-per-eval-prompt 1 "
         "--eval-max-response-len 1024 "

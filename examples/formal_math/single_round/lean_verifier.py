4d786db3c90e52357ebaf78ac41b524948de7467
fzyzcjy <ch271828n@outlook.com>
2025-10-15 16:26:36 +0800

    more

--- examples/formal_math/single_round/lean_verifier.py
+++ examples/formal_math/single_round/lean_verifier.py
@@ -1,8 +1,18 @@
 import kimina_client
 
+
 class LeanVerifier:
     def __init__(self):
         TODO
 
     def check(self):
         return TODO
+
+
+# TODO handle docker stop more gracefully
+class _KiminaServerWrapper:
+    def __init__(self):
+        self._docker_start()
+
+    def _docker_start(self):
+        TODO

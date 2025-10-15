a0070688c048c0111935fe3334bfccd2a71d4317
fzyzcjy <ch271828n@outlook.com>
2025-10-15 16:52:39 +0800

    more

--- examples/formal_math/single_round/kimina_wrapper.py
+++ examples/formal_math/single_round/kimina_wrapper.py
@@ -6,21 +6,25 @@ from kimina_client import AsyncKiminaClient, CheckResponse
 class KiminaServerAndClientCluster:
     def __init__(self):
         self._servers = _create_servers()
-        self._client_cluster = _KiminaClientCluster(TODO)
+        self._client_cluster = _KiminaClientCluster(self._servers)
 
     async def check(self, *args, **kwargs) -> CheckResponse:
         return await self._client_cluster.check(*args, **kwargs)
 
 
 class _KiminaClientCluster:
-    def __init__(self):
-        self._clients = [AsyncKiminaClient(TODO) for TODO in TODO]
+    def __init__(self, servers: List["_KiminaServer"]):
+        self._clients = [AsyncKiminaClient(api_url=server.api_url) for server in servers]
         self._next_client_index = 0
 
     async def check(self, *args, **kwargs):
         client = self._clients[self._next_client_index]
         self._next_client_index = (self._next_client_index + 1) % len(self._clients)
         return await client.check(*args, **kwargs)
+
+
+def _create_servers() -> List["_KiminaServer"]:
+    return TODO
 
 
 # TODO handle docker stop more gracefully
@@ -30,3 +34,7 @@ class _KiminaServer:
 
     def _docker_start(self):
         TODO
+
+    @property
+    def api_url(self):
+        return TODO

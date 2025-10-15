7db0ffaca53df9052f478a63006c19d1264dc150
fzyzcjy <ch271828n@outlook.com>
2025-10-15 15:59:28 +0800

    more

--- tests/command_utils.py
+++ tests/command_utils.py
@@ -100,7 +100,10 @@ def get_default_wandb_args(test_file: str, run_name_prefix: Optional[str] = None
         print("Skip wandb configuration since WANDB_API_KEY is not found")
         return ""
 
-    test_name = Path(test_file).stem
+    test_file = Path(test_file)
+    test_name = test_file.stem
+    if len(test_name) < 6:
+        test_name = f"{test_file.parent.name}_{test_name}"
 
     run_name = f"{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}-{random.randint(0, 1000000000)}"
     if (x := os.environ.get("GITHUB_COMMIT_NAME")) is not None:

ef45fdea08c0cdd65be0a01fa7b90a4b2daf4d72
fzyzcjy <ch271828n@outlook.com>
2025-10-16 17:22:51 +0800

    more

--- examples/formal_math/single_round/run_sft.py
+++ examples/formal_math/single_round/run_sft.py
@@ -26,8 +26,8 @@ def execute():
 
     load_save_path = f"/root/models/{MODEL_NAME}_ckpt__{Path(__file__).stem}_{run_id}/"
     ckpt_args = (
-        f"--hf-checkpoint /root/{MODEL_NAME}/ "
-        f"--ref-load /root/{MODEL_NAME}_torch_dist "
+        f"--hf-checkpoint /root/models/{MODEL_NAME}/ "
+        f"--ref-load /root/models/{MODEL_NAME}_torch_dist "
         f"--load {load_save_path} "
         f"--save {load_save_path} "
         "--save-interval 1000 "

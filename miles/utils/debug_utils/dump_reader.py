5a0726ab8e144bee399b8584b24b9b6d9b6634dd
fzyzcjy <ch271828n@outlook.com>
2025-10-16 11:49:30 +0800

    more

--- miles/utils/debug_utils/dump_reader.py
+++ miles/utils/debug_utils/dump_reader.py
@@ -1,3 +1,4 @@
+import polars as pl
 import torch
 from miles.utils.types import Sample
 
@@ -5,7 +6,9 @@ from miles.utils.types import Sample
 def read_rollout(rollout_data_path: str):
     pack = torch.load(rollout_data_path)
     samples = [Sample.from_dict(s) for s in pack["samples"]]
+    df_samples = pl.Dataframe(pack["samples"])
     return dict(
         pack=pack,
         samples=samples,
+        df_samples=df_samples,
     )

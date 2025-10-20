01e1e757b44d2d919cb60850bbb5ea66ac9d93d3
fzyzcjy <ch271828n@outlook.com>
2025-10-20 09:33:21 +0800

    more

--- scripts/run_qwen3_30b_a3b.py
+++ scripts/run_qwen3_30b_a3b.py
@@ -146,7 +146,8 @@ def execute():
                 "--rollout-num-gpus-per-engine 4 "
                 # fused_moe_kernel triton seems to have issue on GB300
                 "--sglang-ep-size 4 "
-                "--sglang-mem-fraction-static 0.7 "
+                # TODO examine why it often OOM
+                "--sglang-mem-fraction-static 0.5 "
                 "--sglang-cuda-graph-bs 1 2 4 8 " + " ".join(str(x) for x in range(16, 513, 8)) + " "
             )
             misc_args += "--actor-num-gpus-per-node 4 "
@@ -162,7 +163,8 @@ def execute():
             sglang_args = (
                 "--rollout-num-gpus-per-engine 4 "
                 "--sglang-ep-size 4 "
-                "--sglang-mem-fraction-static 0.7 "
+                # TODO examine why it often OOM
+                "--sglang-mem-fraction-static 0.5 "
                 "--sglang-cuda-graph-bs 1 2 4 8 " + " ".join(str(x) for x in range(16, 513, 8)) + " "
             )
             misc_args += "--actor-num-gpus-per-node 4 "

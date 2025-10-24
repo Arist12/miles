162572a1dca7f6ff1cc20c53cc3c8e2b58e7c7dc
fzyzcjy <ch271828n@outlook.com>
2025-10-24 08:45:56 +0800

    cp

--- scripts/run_qwen3_4b_fsdp.py
+++ scripts/run_qwen3_4b_fsdp.py
@@ -20,15 +20,15 @@ def prepare():
 
 
 def execute():
-    load_save_path = (
-        f"/root/models/{MODEL_NAME}_ckpt__{Path(__file__).stem}__{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}/"
-    )
+    # load_save_path = (
+    #     f"/root/models/{MODEL_NAME}_ckpt__{Path(__file__).stem}__{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}/"
+    # )
     ckpt_args = (
         f"--hf-checkpoint /root/models/{MODEL_NAME}/ "
-        f"--ref-load /root/{MODEL_NAME}_torch_dist "
-        f"--load {load_save_path} "
-        f"--save {load_save_path} "
-        "--save-interval 20 "
+        # f"--ref-load /root/{MODEL_NAME}_torch_dist "
+        # f"--load {load_save_path} "
+        # f"--save {load_save_path} "
+        # "--save-interval 20 "
     )
 
     rollout_args = (
@@ -56,27 +56,19 @@ def execute():
     )
 
     perf_args = (
-        "--tensor-model-parallel-size 2 "
-        "--sequence-parallel "
-        "--pipeline-model-parallel-size 1 "
-        "--context-parallel-size 1 "
-        "--expert-model-parallel-size 1 "
-        "--expert-tensor-parallel-size 1 "
-        "--recompute-granularity full "
-        "--recompute-method uniform "
-        "--recompute-num-layers 1 "
         "--use-dynamic-batch-size "
-        "--max-tokens-per-gpu 9216 "
+        "--max-tokens-per-gpu 4096 "
     )
 
     grpo_args = (
         "--advantage-estimator grpo "
-        "--use-kl-loss "
+        # "--use-kl-loss "
         "--kl-loss-coef 0.00 "
         "--kl-loss-type low_var_kl "
         "--entropy-coef 0.00 "
         "--eps-clip 0.2 "
         "--eps-clip-high 0.28 "
+        "--use-tis "
     )
 
     optimizer_args = (
@@ -89,23 +81,16 @@ def execute():
     )
 
     sglang_args = (
-        # "--rollout-num-gpus-per-engine 2 "
-        f"--rollout-num-gpus-per-engine 8 "  # temp use 1 engine per node to avoid flashinfer err
+        "--rollout-num-gpus-per-engine 1 "
         "--sglang-mem-fraction-static 0.7 "
     )
 
-    misc_args = (
-        # default dropout in megatron is 0.1
-        "--attention-dropout 0.0 "
-        "--hidden-dropout 0.0 "
-        # should be good for model performance
-        "--accumulate-allreduce-grads-in-fp32 "
-        "--attention-softmax-in-fp32 "
-        # need to comment this when using model with MLA
-        "--attention-backend flash "
-        "--actor-num-nodes 1 "
-        "--actor-num-gpus-per-node 8 "
-        "--colocate "
+    fsdp_args = (
+        "--train-backend fsdp "
+        # "--gradient-checkpointing "
+        # "--attn-implementation flash_attention_2 "
+        f"--update-weights-bucket-size {512 * 1024 * 1024} "
+        # "--fsdp-full-params "
     )
 
     train_args = (
@@ -117,7 +102,7 @@ def execute():
         f"{perf_args} "
         f"{eval_args} "
         f"{sglang_args} "
-        f"{misc_args} "
+        f"{fsdp_args} "
     )
 
     U.execute_train(

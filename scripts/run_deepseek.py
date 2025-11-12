28dbd08714ac76e63b30f467c52c179c49c83560
fzyzcjy <ch271828n@outlook.com>
2025-11-12 17:28:00 +0800

    hack

--- scripts/run_deepseek.py
+++ scripts/run_deepseek.py
@@ -228,7 +228,9 @@ def train(args: ScriptArgs):
         "--recompute-num-layers 1 "
         # ------------
         "--use-dynamic-batch-size "
-        "--max-tokens-per-gpu 16384 "
+        # TODO temp use tiny value
+        "--max-tokens-per-gpu 2048 "
+        # "--max-tokens-per-gpu 16384 "
     )
 
     grpo_args = (
@@ -252,7 +254,7 @@ def train(args: ScriptArgs):
         # ------------
         # "--optimizer-cpu-offload "
         # "--overlap-cpu-optimizer-d2h-h2d "
-        # "--use-precision-aware-optimizer "
+        "--use-precision-aware-optimizer "
     )
 
     sglang_decode_max_bs = 256

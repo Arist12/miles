90d2db7bd941b2abda495f94f2e45bbdea16e720
fzyzcjy <ch271828n@outlook.com>
2025-11-14 10:49:11 +0800

    more

--- tests/ci/github_runner/entrypoint.sh
+++ tests/ci/github_runner/entrypoint.sh
@@ -5,14 +5,16 @@ set -eux
 # ref: https://github.com/actions/runner
 # args ref: https://github.com/actions/runner/blob/68ff57dbc4c836d50f46602a8a53301fb9513eb4/src/Runner.Listener/CommandSettings.cs#L53
 
-DIR="/data/miles_ci/runner_$(hostname)"
-FLAG="$DIR/miles_runner_config_executed.txt"
+DIR_BASE="/data/miles_ci/runner_$(hostname)"
+cd $DIR_BASE
 
-mkdir -p "$DIR"
+CONFIG_EXECUTED_FLAG_PATH="$DIR_BASE/miles_runner_config_executed.txt"
 
-if [ ! -f "$FLAG" ]; then
-  /home/runner/config.sh --url ${GITHUB_RUNNER_URL} --token ${GITHUB_RUNNER_TOKEN} --unattended --work "$DIR"
-  echo "configured" > "$FLAG"
+mkdir -p "$DIR_BASE"
+
+if [ ! -f "$CONFIG_EXECUTED_FLAG_PATH" ]; then
+  /home/runner/config.sh --url ${GITHUB_RUNNER_URL} --token ${GITHUB_RUNNER_TOKEN} --unattended --work "$DIR_BASE/github_actions_work"
+  echo "configured" > "$CONFIG_EXECUTED_FLAG_PATH"
 else
   echo "config.sh already executed, skipping"
 fi

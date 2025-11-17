e2d1af86f8b8782e7685f78697a9a4cdefe036ab
fzyzcjy <ch271828n@outlook.com>
2025-11-17 21:46:12 +0800

    more

---
+++ miles/utils/tracking_utils.py
@@ -0,0 +1,9 @@
+from . import wandb_utils
+
+
+def init_tracking(args, primary: bool = True, **kwargs):
+    if primary:
+        wandb_utils.init_wandb_primary(args, **kwargs)
+    else:
+        wandb_utils.init_wandb_secondary(args, **kwargs)
+    TODO

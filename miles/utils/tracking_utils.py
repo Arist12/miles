d7906a3d8ce0f6f8edc1e2ce29af2bb15c1574ae
fzyzcjy <ch271828n@outlook.com>
2025-11-17 21:53:23 +0800

    more

--- miles/utils/tracking_utils.py
+++ miles/utils/tracking_utils.py
@@ -11,7 +11,7 @@ def init_tracking(args, primary: bool = True, **kwargs):
         wandb_utils.init_wandb_secondary(args, **kwargs)
 
 
-# TODO further refactor
+# TODO further refactor, e.g. put TensorBoard init to the "init" part
 def log(args, metrics, step_key: str):
     if args.use_wandb:
         wandb.log(metrics)

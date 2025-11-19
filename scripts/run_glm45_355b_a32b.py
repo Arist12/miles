a37d761fb60328ff3c5c1ed22b7257c3f8e5bb37
fzyzcjy <ch271828n@outlook.com>
2025-11-19 10:35:32 +0800

    more

--- scripts/run_glm45_355b_a32b.py
+++ scripts/run_glm45_355b_a32b.py
@@ -220,7 +220,7 @@ def train(args: ScriptArgs):
         "--accumulate-allreduce-grads-in-fp32 "
         "--attention-softmax-in-fp32 "
         # need to comment this when using model with MLA
-        # "--attention-backend flash "
+        "--attention-backend flash "
         f"--update-weight-buffer-size {4 * 1024 ** 3} "
         # TODO maybe enable it
         # use deepep for megatron

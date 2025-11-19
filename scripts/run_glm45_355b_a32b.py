69248c02043562a49a395e7d08c6a3f1a7556d1d
fzyzcjy <ch271828n@outlook.com>
2025-11-19 10:10:34 +0800

    more

--- scripts/run_glm45_355b_a32b.py
+++ scripts/run_glm45_355b_a32b.py
@@ -45,59 +45,6 @@ def prepare_spmd(args: ScriptArgs):
     _convert_to_megatron_ckpt(args)
 
 
-def _convert_to_megatron_ckpt(args: ScriptArgs):
-    """This script needs to be executed once per node."""
-    path_dst = f"/root/models/{args.model_name}_torch_dist"
-    if Path(path_dst).exists():
-        return
-
-    # `export SLURM_JOB_HOSTNAMES=$(scontrol show hostnames "$SLURM_JOB_NODELIST")`
-    print(f"{os.environ.get('SLURM_JOB_HOSTNAMES')=} {os.environ.get('SLURM_NODEID')=}")
-    master_addr = os.environ["SLURM_JOB_HOSTNAMES"].split("\n")[0]
-    node_rank = int(os.environ["SLURM_NODEID"])
-
-    cmd = (
-        f"source scripts/models/{args.megatron_model_type}.sh && "
-        "PYTHONPATH=/root/Megatron-LM/ torchrun "
-        f"--nproc-per-node {args.num_gpus_per_node} "
-        f"--master-addr {master_addr} "
-        "--master-port 23456 "
-        f"--nnodes={args.num_nodes} "
-        f"--node-rank {node_rank} "
-        "tools/convert_hf_to_torch_dist.py "
-        "${MODEL_ARGS[@]} "
-        f"--hf-checkpoint /root/models/{args.model_name}-bf16/ "
-        f"--save {path_dst} "
-    )
-
-    # TODO unify 5layer w/ 20layer
-    if args.num_nodes == 1 and args.model_name == "DeepSeek-V3-0324-5layer":
-        cmd += (
-            "--tensor-model-parallel-size 1 "
-            "--pipeline-model-parallel-size 1 "
-            "--expert-tensor-parallel-size 1 "
-            "--expert-model-parallel-size 1 "
-        )
-    elif args.model_name == "DeepSeek-V3-0324-20layer":
-        cmd += (
-            "--tensor-model-parallel-size 1 "
-            "--expert-tensor-parallel-size 1 "
-            "--expert-model-parallel-size 4 "
-            # PP info will be auto determined by converter script
-        )
-    else:
-        cmd += (
-            "--tensor-model-parallel-size 1 "
-            "--pipeline-model-parallel-size 8 "
-            "--expert-tensor-parallel-size 1 "
-            "--expert-model-parallel-size 4 "
-            "--decoder-first-pipeline-num-layers 7 "
-            "--decoder-last-pipeline-num-layers 6 "
-        )
-
-    U.exec_command(cmd)
-
-
 # TODO improve these commadns
 @app.command()
 @U.dataclass_cli

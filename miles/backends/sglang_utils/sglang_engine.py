1a3fd7319cfb8b6bdd817ef3f6803fab95d87951
Yueming Yuan <yym022502@gmail.com>
2026-09-23 15:43:01 -0700

    [refactor] (mechanical move) group LoRA and HF utilities into subdirectories (#3620)

--- miles/backends/sglang_utils/sglang_engine.py
+++ miles/backends/sglang_utils/sglang_engine.py
@@ -4,18 +4,17 @@ import os
 import shlex
 import sys
 
-
 from sglang.srt.server_args import ServerArgs
 
 from miles.backends.megatron_utils.lora.utils import convert_target_modules_to_hf, sglang_lora_target_all_sentinel
 from miles.backends.sglang_utils.server_args_utils import server_args_to_argv
-from miles.utils.lora import (
+from miles.utils.lora.utils import (
     LORA_ADAPTER_NAME,
     engine_loads_adapter_from_disk,
+    is_multi_lora_enabled,
     lora_base_cpu_backup_enabled,
     lora_rollout_enabled,
 )
-from miles.utils.multi_lora import is_multi_lora_enabled
 from miles.utils.workers.argv_utils import _record_field_names
 
 logger = logging.getLogger(__name__)
